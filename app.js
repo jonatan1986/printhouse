e67af0a959540465121f4989e9ea78d048f5bed7
jonatan_ganpul <buga1986@gmail.com>
2020-04-27 02:45:21 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+/**
+ * Module dependencies.
+ */
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var logger = require('morgan');
+var connect = require('connect');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var errorHandler = require('errorhandler');
+var router = express.Router();
+var app = express();
+
+//=============GET Variants================
+var homepage = require('./routes/homepage');
+app.get('/homepage',homepage.form);
+
+//==========================================
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser('your secret here'));
+app.use(session({
+    name: 'sid',
+	saveUninitialized: false,
+	resave: false,
+	secret: `quiet, pal! it's a secret!`,
+	cookie: {
+	maxAge: 1000 * 60 * 60 * 2,
+	sameSite: true,
+	}
+}));
+
+app.use(methodOverride());
+app.use(express.static(path.join(__dirname, '/public')));
+
+router.use(function (req, res, next){
+// console.log("router js res.statuscode down ",res.statusCode);
+// next();
+});
+// development only
+
+
+
+
+app.use(errorHandler);
+
+if ('development' == app.get('env')) {
+  app.use(errorHandler);
+}
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ readme.txt
@@ -0,0 +1,1 @@
+sudo npm --build-from-source install bcrypt@3.0.6
