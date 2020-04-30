af50446663fdf7c54dc7a4bd88b3c6593b1f9be7
jonatan_ganpul <buga1986@gmail.com>
2020-04-30 05:26:55 -0700

    postcards are located and aligned

--- routes/design.js
+++ routes/design.js
@@ -1,11 +1,27 @@
+var photoArray = [];
+
+photoArray.push({
+  name:'birds',
+  path:'postcard_birdsandflowers.jpg'
+});
+
+photoArray.push({
+  name:'leaves',
+  path:'postcard_leaves.jpg'
+});
+
+photoArray.push({
+  name:'offwhite',
+  path:'postcard_offwhite.jpg'
+});
+
 exports.form = function(req, res){
   console.log(req.session.uid);
-  res.render('design', { title: 'ברוך הבא'});
+  res.render('design', { title: 'ברוך הבא',Photos:photoArray});
 };
 
 
 exports.logout = function(req, res){
-  console.log("logout");
   req.session.destroy(function(err) {
     if (err) throw err;
     res.redirect('/login');
