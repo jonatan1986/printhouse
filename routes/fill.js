10ded37548955438397415e5f7b98edd16749cf6
jonatan_ganpul <buga1986@gmail.com>
2020-05-14 10:54:03 -0700

    fill page contain the option to select amount of postcards & data is sent to submit post request & fill page expends upon displaying the result

--- routes/fill.js
+++ routes/fill.js
@@ -1,6 +1,25 @@
-
+var Photo = require('../models/Photo');
 
 
 exports.form = function(req, res){
   res.render('fill', { title: ''});
+};
+
+
+exports.save = function(req, res){
+  console.log("fill post save req.body",req.body.photodata.title);
+  console.log("fill post save req.name",req.user.name);
+  // Photo.create({
+  //   username: req.user.name,
+  //   title: req.body.photodata.title,
+  //   content: req.body.photodata.content,
+  //   filepath: req.body.photodata.path
+  // },
+  // function(err) {
+  //   if (err)
+  //   {
+  //     return next(err);
+  //   }
+  // });
+  res.redirect('/order');
 };
